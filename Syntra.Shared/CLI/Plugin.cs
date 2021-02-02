b038ecb3f9f1533e0c81b655e62414d28fc7e7a0
Filip Geens <filipg@waegener.com>
2021-02-02 17:07:21 +0100

    CLI base added AppTools with default app data directory location added

---
+++ Syntra.Shared/CLI/Plugin.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Syntra.Shared.CLI {
+	public interface ICliPlugin {
+		string Name { get; }
+		bool Execute(CLIBase parent, CliCommand Input);
+		void ShowHelp();
+	}
+}
---
+++ Syntra.Shared/Tools/AppTools.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace Syntra.Shared.Tools {
+  public class AppTools {
+    public static string GetAppWorkDirectory(string workdir=null) {
+      workdir??=Assembly.GetExecutingAssembly().GetName().Name;
+      string dir = $@"{Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData).TrimEnd('\\')}\{workdir}";
+      if (!Directory.Exists(dir)) { try { Directory.CreateDirectory(dir); } catch { } }
+      return dir;
+    }
+  }
+}
